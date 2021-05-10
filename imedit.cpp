23e9f06b4b17849726e1cc073a89dfd96b0c7235
lyxell <alyxell@kth.se>
2021-05-10 20:52:21 +0200

    Return true on click for highlight and underline

--- imedit.cpp
+++ imedit.cpp
@@ -1,12 +1,7 @@
 #include "imedit.h"
 #include "imgui.h"
 
-#include <functional>
 #include <string>
-#include <unordered_map>
-#include <utility>
-#include <vector>
-#include <iostream>
 
 namespace ImEdit {
 
@@ -14,15 +9,36 @@ static const float number_column_width = 20.0f;
 static std::string current_editor;
 static int current_row;
 
+ImVec2 get_upper_left(int x0, int x1) {
+    ImGuiStyle& style = ImGui::GetStyle();
+    return ImVec2(ImGui::GetCursorScreenPos().x +
+                      ImGui::CalcTextSize("x").x * x0 + number_column_width +
+                      style.CellPadding.x * 2.0f,
+                  ImGui::GetCursorScreenPos().y + style.CellPadding.y);
+}
+ImVec2 get_lower_right(int x0, int x1) {
+    auto upper_left = get_upper_left(x0, x1);
+    return ImVec2(upper_left.x + ImGui::CalcTextSize("x").x * (x1 - x0),
+                  upper_left.y + ImGui::GetTextLineHeight());
+}
+ImVec2 get_lower_left(int x0, int x1) {
+    ImGuiStyle& style = ImGui::GetStyle();
+    return ImVec2(ImGui::GetCursorScreenPos().x +
+                      ImGui::CalcTextSize("x").x * x0 + number_column_width +
+                      style.CellPadding.x * 2.0f,
+                  ImGui::GetCursorScreenPos().y + style.CellPadding.y +
+                      ImGui::GetTextLineHeight());
+}
+
 void Begin(const char* str) { current_editor = str; }
 
 void Line(const char* str) {
     constexpr int COLUMNS = 2;
     current_row++;
     static ImGuiTableFlags flags = ImGuiTableFlags_SizingFixedFit;
-    if (ImGui::BeginTable((current_editor + "#Line").c_str(), COLUMNS,
-                          flags)) {
-        ImGui::TableSetupColumn((current_editor + "#number_column").c_str(), 0, number_column_width);
+    if (ImGui::BeginTable((current_editor + "#Line").c_str(), COLUMNS, flags)) {
+        ImGui::TableSetupColumn((current_editor + "#number_column").c_str(), 0,
+                                number_column_width);
         ImGui::TableSetupColumn((current_editor + "#text_column").c_str(),
                                 ImGuiTableColumnFlags_WidthStretch);
         ImGui::TableNextColumn();
@@ -33,63 +49,53 @@ void Line(const char* str) {
     }
 }
 
-void Highlight(int x0, int x1) {
-    ImGuiStyle& style = ImGui::GetStyle();
+bool Highlight(int x0, int x1) {
     auto* draw_list = ImGui::GetWindowDrawList();
-    auto upper_left = ImVec2(
-        ImGui::GetCursorScreenPos().x + ImGui::CalcTextSize("x").x * x0 + number_column_width + style.CellPadding.x * 2.0f,
-        ImGui::GetCursorScreenPos().y + style.CellPadding.y
-    );
-    auto lower_right =
-        ImVec2(upper_left.x + ImGui::CalcTextSize("x").x * (x1 - x0),
-               upper_left.y + ImGui::GetTextLineHeight());
+    auto upper_left = get_upper_left(x0, x1);
+    auto lower_right = get_lower_right(x0, x1);
     draw_list->AddRectFilled(upper_left, lower_right,
-                            IM_COL32(150, 200, 255, 255));
+                             IM_COL32(150, 200, 255, 255));
+    return ImGui::IsMouseHoveringRect(upper_left, lower_right) &&
+           ImGui::IsMouseClicked(0);
 }
 
-void Underline(int x0, int x1) {
+bool Underline(int x0, int x1) {
     ImGuiStyle& style = ImGui::GetStyle();
     auto* draw_list = ImGui::GetWindowDrawList();
-    auto curr = ImVec2(
-        ImGui::GetCursorScreenPos().x + ImGui::CalcTextSize("x").x * x0 + number_column_width + style.CellPadding.x * 2.0f,
-        ImGui::GetCursorScreenPos().y + style.CellPadding.y + ImGui::GetTextLineHeight()
-    );
+    auto curr = get_lower_left(x0, x1);
     auto end_x = curr.x + ImGui::CalcTextSize("x").x * (x1 - x0);
     auto incr_x = 2.0f;
     bool bump = 0;
     while (curr.x < end_x) {
         if (bump) {
-            draw_list->AddLine(ImVec2(curr.x,          curr.y),
+            draw_list->AddLine(ImVec2(curr.x, curr.y),
                                ImVec2(curr.x + incr_x, curr.y + 1.0f),
-                               IM_COL32(255, 0, 0, 255),
-                               1.0f);
+                               IM_COL32(255, 0, 0, 255), 1.0f);
         } else {
-            draw_list->AddLine(ImVec2(curr.x,          curr.y + 1.0f),
+            draw_list->AddLine(ImVec2(curr.x, curr.y + 1.0f),
                                ImVec2(curr.x + incr_x, curr.y),
-                               IM_COL32(255, 0, 0, 255),
-                               1.0f);
+                               IM_COL32(255, 0, 0, 255), 1.0f);
         }
         curr.x += incr_x;
         bump = !bump;
     }
+    return ImGui::IsMouseHoveringRect(get_upper_left(x0, x1),
+                                      get_lower_right(x0, x1)) &&
+           ImGui::IsMouseClicked(0);
 }
 
 void Cursor(int x) {
     ImGuiStyle& style = ImGui::GetStyle();
     auto* draw_list = ImGui::GetWindowDrawList();
-    auto upper_left = ImVec2(
-        ImGui::GetCursorScreenPos().x + ImGui::CalcTextSize("x").x * x + number_column_width + style.CellPadding.x * 2.0f,
-        ImGui::GetCursorScreenPos().y + style.CellPadding.y
-    );
+    auto upper_left =
+        ImVec2(ImGui::GetCursorScreenPos().x + ImGui::CalcTextSize("x").x * x +
+                   number_column_width + style.CellPadding.x * 2.0f,
+               ImGui::GetCursorScreenPos().y + style.CellPadding.y);
     auto lower_right =
-        ImVec2(upper_left.x + 1.0f,
-               upper_left.y + ImGui::GetTextLineHeight());
-    draw_list->AddRectFilled(upper_left, lower_right,
-                            IM_COL32(0, 0, 0, 255));
+        ImVec2(upper_left.x + 1.0f, upper_left.y + ImGui::GetTextLineHeight());
+    draw_list->AddRectFilled(upper_left, lower_right, IM_COL32(0, 0, 0, 255));
 }
 
-void End() {
-    current_row = 0;
-}
+void End() { current_row = 0; }
 
 } // namespace ImEdit
--- imedit.h
+++ imedit.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#include <functional>
-
 namespace ImEdit {
 
 void Begin(const char* name);
 void Line(const char* content);
 void Cursor(int x);
-void Highlight(int x0, int x1);
-void Underline(int x0, int x1);
+bool Highlight(int x0, int x1);
+bool Underline(int x0, int x1);
 void End();
 
 } // namespace ImEdit
